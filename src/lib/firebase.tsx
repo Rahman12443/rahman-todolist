2d8cfca88b03f4df1091b32183d2d57da3b4ebe5
Rahman <rahmanfadillah23gmail.com>
2025-04-14 09:41:12 +0700

    First commit

---
+++ src/lib/firebase.tsx
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// isi konfigurasi sesuai dengan konfigurasi firebase kalian
+const firebaseConfig = {
+  apiKey: "AIzaSyCpb3cs_3UQUb7VWmaJUQ32Z76ZDGoy2Yg",
+  authDomain: "project-todolist-6c152.firebaseapp.com",
+  projectId: "project-todolist-6c152",
+  storageBucket: "project-todolist-6c152.firebasestorage.app",
+  messagingSenderId: "1078114853551",
+  appId: "1:1078114853551:web:c6ad96ba36b21b1f482a5a",
+  measurementId: "G-DGR6864CGW"
+};
+
+// Inisialisasi Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
+
