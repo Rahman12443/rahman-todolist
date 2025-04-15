612524186f74955eeccd7320d71cdbd55ba0f718
Rahman <rahmanfadillah23gmail.com>
2025-04-15 22:10:21 +0700

    penyesuaian

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Todolist from '@/app/c';
 import React from 'react';
 
 function page() {
   return (
     <>
       <h1>Landing Page </h1>
+      <Todolist/>
     </>
   );
 }
--- src/lib/firebase.tsx
+++ src/lib/firebase.tsx
@@ -3,13 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // isi konfigurasi sesuai dengan konfigurasi firebase kalian
 const firebaseConfig = {
-  apiKey: "AIzaSyCpb3cs_3UQUb7VWmaJUQ32Z76ZDGoy2Yg",
-  authDomain: "project-todolist-6c152.firebaseapp.com",
-  projectId: "project-todolist-6c152",
-  storageBucket: "project-todolist-6c152.firebasestorage.app",
-  messagingSenderId: "1078114853551",
-  appId: "1:1078114853551:web:c6ad96ba36b21b1f482a5a",
-  measurementId: "G-DGR6864CGW"
+  apiKey: 'AIzaSyCpb3cs_3UQUb7VWmaJUQ32Z76ZDGoy2Yg',
+  authDomain: 'project-todolist-6c152.firebaseapp.com',
+  projectId: 'project-todolist-6c152',
+  storageBucket: 'project-todolist-6c152.firebasestorage.app',
+  messagingSenderId: '1078114853551',
+  appId: '1:1078114853551:web:c6ad96ba36b21b1f482a5a',
 };
 
 // Inisialisasi Firebase
@@ -17,4 +16,3 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
-
