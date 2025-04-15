66a75185b211a73c8093e53e1dab34bf582a4e5d
Rahman <rahmanfadillah23gmail.com>
2025-04-15 22:46:03 +0700

    perbaruan

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Todolist from '@/app/c';
 import React from 'react';
+import TodoList from '../components/todolist';
 
 function page() {
   return (
     <>
-      <h1>Landing Page </h1>
-      <Todolist/>
+      <TodoList/>
     </>
   );
 }
